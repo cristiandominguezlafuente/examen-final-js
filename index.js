61d9d31139b11fa369a5f119b46d280d55a6b682
Craiaaan <dominguezlafuentecristian@fpllefia.com>
2026-05-26 12:04:41 +0200

    dsaada

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+//Array
+
+let platosDisponibles = [
+  {
+    nombre: "Ensalada César",
+    descripcion: "Ensalada con pollo, lechuga, queso y salsa César",
+    tipo: "primero",
+    precio: 8.5,
+    foto: "ensalada.jpg",
+  },
+  {
+    nombre: "Macarrones boloñesa",
+    descripcion: "Pasta con salsa de tomate y carne picada",
+    tipo: "primero",
+    precio: 9.0,
+    foto: "macarrones.jpg",
+  },
+  {
+    nombre: "Pollo al horno",
+    descripcion: "Pollo asado con patatas",
+    tipo: "segundo",
+    precio: 11.5,
+    foto: "pollo.jpg",
+  },
+  {
+    nombre: "Merluza a la plancha",
+    descripcion: "Merluza con verduras salteadas",
+    tipo: "segundo",
+    precio: 12.0,
+    foto: "merluza.jpg",
+  },
+];
+
 //Ej 1
 
 let cabeceraMenu = document.querySelector("#cabeceraMenu");
@@ -9,6 +42,7 @@ function cambiarFraseDelDia() {
 fraseDia.addEventListener("input", cambiarFraseDelDia);
 
 // Ej 2
+
 let selectorDia = document.querySelector("#selectorDia");
 
 function seleccionarDia() {
